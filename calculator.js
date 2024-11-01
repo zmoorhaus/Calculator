501ef628fb1866f4820479bdc5027a6961f0a6c4
Zachariah Moorhaus <zmoorhaus@gmail.com>
2024-11-01 10:34:34 -0400

    create functions of 4 basic operators

--- calculator.js
+++ calculator.js
@@ -1,1 +1,17 @@
 console.log("its time to calculate!!!")
+
+let add = function(a, b) {
+    return (a + b);
+}
+
+let subtract = function(a, b) {
+    return (a - b);
+};
+
+let multiply = function(a, b) {
+    return (a * b)
+}
+
+let divide = function(a, b) {
+    return (a / b)
+}
